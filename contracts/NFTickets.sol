7aa8ed306b3eda63227ce519f4fb1bf153df86a8
xhawla <69622217+khawlahssn@users.noreply.github.com>
2022-01-11 00:05:03 +0400

    Adds contract functions

--- contracts/NFTickets.sol
+++ contracts/NFTickets.sol
@@ -7,4 +7,33 @@ pragma solidity 0.8.8;
 
 contract NFTickets {
 
+  // Needs more properties
+  struct TicketDetails {
+    uint256 purchasePrice;
+    uint256 sellingPrice;
+    bool forSale;
+  }
+
+  // Modifiers
+  modifier onlyOwner {}
+  modifier isEventManager {}
+  modifier isAttendee {}
+  
+  // Functions
+  function createEvent() isEventManager {}
+  function disableEvent() onlyOwner {}
+  function buyTickets() {}
+  function requestRefund() isAttendee {}
+  function refundAttendee() isEventManager {}
+  function refundAll() isEventManager {}
+  function setCommissionPercentage() onlyOwner {}
+  function withdrawCommissions() onlyOwner {}
+  
+  function getTicketPrice() view returns () {}
+  function getRemainingTickets() view returns() {}
+  function getOrganizers() view returns () {}
+  function getTicketCount() view returns () {} // is there a getBalanceOf() function that we can use?
+  function getAllEvents() view returns () {}
+  
+
 }
